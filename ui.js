afae157635cac1de2b2459c24fcdd82edb4e9d6c
kevinbruggework-dotcom <kevinbrugge.work@gmail.com>
2026-04-24 03:57:11 +0200

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,11 +1,1 @@
-function addMessage(sender, text) {
-  const box = document.getElementById("chatBox");
 
-  const div = document.createElement("div");
-  div.className = "msg";
-
-  div.innerHTML = `<b>${sender}</b>: ${text}`;
-
-  box.appendChild(div);
-  box.scrollTop = box.scrollHeight;
-}
